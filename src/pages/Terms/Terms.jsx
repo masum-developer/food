8779291298bcc28c5bce33bd6e3caa9ba332c526
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-05-05 20:19:20 +0600

    favicon update

--- src/pages/Terms/Terms.jsx
+++ src/pages/Terms/Terms.jsx
@@ -3,8 +3,39 @@ import { Link } from 'react-router-dom';
 
 const Terms = () => {
     return (
-        <div>
+        <div className='container'>
             <h2>Terms And Condition</h2>
+            <p>
+                
+
+Welcome to Chefs Corner! 
+
+These terms and conditions outline the rules and regulations for the use of our website. By accessing or using Chef's Recipe Website, you agree to be bound by these terms and conditions.
+
+Intellectual Property Rights
+All content included on our website, such as text, graphics, logos, images, and software, is the property of Chef's Recipe or its content suppliers and is protected by international copyright laws. You may not reproduce, copy, distribute, or exploit any part of our website without our prior written permission.
+
+User Content
+Our website allows users to post their own content, such as comments and recipes. By posting content, you grant Chef's Recipe a non-exclusive, royalty-free, perpetual, and irrevocable right to use, reproduce, modify, adapt, publish, translate, and distribute the content worldwide in any media.
+
+User Conduct
+You agree not to use our website for any unlawful purpose or in any way that could damage, disable, overburden, or impair our website or interfere with any other party's use and enjoyment of our website. You also agree not to use our website to distribute unsolicited commercial messages or spam.
+
+Disclaimer
+Chef's Recipe does not warrant that our website will be uninterrupted or error-free or that defects will be corrected. We do not make any representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose.
+
+Limitation of Liability
+Chef's Recipe will not be liable for any direct, indirect, incidental, consequential, or punitive damages arising out of or relating to your use or inability to use our website, even if we have been advised of the possibility of such damages.
+
+Changes to These Terms and Conditions
+We may revise these terms and conditions from time to time without notice. By continuing to access or use our website, you agree to be bound by the revised terms and conditions.
+
+Governing Law
+These terms and conditions are governed by and construed in accordance with the laws of governing law. Any disputes arising out of or in connection with these terms and conditions will be subject to the exclusive jurisdiction of the courts.
+
+Contact Us
+If you have any questions about these terms and conditions, please contact us at 91 BCC Road Wari-Dhaka 1203.
+            </p>
             <p>Go back to <Link to='/register'>Register</Link> </p>
         </div>
     );
